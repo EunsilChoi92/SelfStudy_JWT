0c900f9e7f386670c2b575512286b532418a353a
eunsil <atheung1@naver.com>
2021-01-13 00:03:17 +0900

    test

---
+++ jwt/src/test/java/com/pisces/jwt/JwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pisces.jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
